8a3ed4224181548b940f0e206d05b27190312cc6
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-16 01:49:54 +0700

    chore(db): modify methode connection database

--- src/dbconn.ts
+++ src/dbconn.ts
@@ -8,10 +8,8 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  connectionLimit: 10, // Adjust the connection limit as needed
 });
 
-// A function to get a connection from the pool
 async function getConnectionFromPool() {
   const connection = await pool.getConnection();
   return connection;
