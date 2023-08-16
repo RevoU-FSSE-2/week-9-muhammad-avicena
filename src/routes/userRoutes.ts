c06a550183386310353fc43a561e9397bf15b976
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-16 13:47:03 +0700

    chore(api): modify routes user api

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,8 +4,8 @@ import { listUser, updateBalanceUser, getUserById, updateUsernameUser } from "..
 const router = express.Router();
 
 router.get("/", listUser);
-router.patch("/:userId", updateBalanceUser);
-router.put("/:userId", updateUsernameUser);
-router.get("/:userId", getUserById);
+router.patch("/:user_id", updateBalanceUser);
+router.put("/:user_id", updateUsernameUser);
+router.get("/:user_id", getUserById);
 
 export default router;
