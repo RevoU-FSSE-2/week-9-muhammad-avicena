dd623ce3577d34d7f47274a29da6cc5216f802de
Hideaki-Ryu <cenarahmant@gmail.com>
2023-08-17 14:08:57 +0700

    chore: modify user controller and user routes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,60 +1,111 @@
-import { Request, Response } from 'express';
-import { userData } from '../db/userData';
+import { Request, Response, NextFunction } from "express";
+import {
+  getListUsersDb,
+  getUsersByIdDb,
+  updateUsernameUserDb,
+  updateBalanceUserDb,
+  updateUserByIdDb,
+} from "../db/userPool";
 
-export const listUser = (req: Request, res: Response) => {
-    const response = {
-        message: 'List of all users',
-        users: userData,
-    };
-    res.status(200).json(response);
+export const listUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const dataUser = await getListUsersDb();
+  res.status(200).json({
+    message: "List of all users",
+    users: dataUser,
+  });
+};
+export const getUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user_id = parseInt(req.params.user_id);
+
+  try {
+    const dataUser: any = await getUsersByIdDb(user_id);
+    if (!dataUser) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User found", user: dataUser });
+  } catch (error) {
+    console.error("Error :", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-export const getUserById = (req: Request, res: Response) => {
-    const userId = parseInt(req.params.userId);
-
-    const user = userData.find(user => user.userId === userId);
-
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.status(200).json({ message: 'User found', user: user });
-};
-
-export const updateUsernameUser = (req: Request, res: Response) => {
-    const userId = parseInt(req.params.userId);
-    const { username } = req.body; 
-
-    const userIndex = userData.findIndex(user => user.userId === userId);
-
-    if (userIndex === -1) {
-        return res.status(404).json({ message: 'User not found' });
-    }
-
-    if (username !== undefined) {
-        userData[userIndex].username = username;
+export const updateUserById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user_id = parseInt(req.params.user_id);
+  const { user_name, user_address, user_email, user_password, user_balance } =
+    req.body;
+  try {
+    const updateUser: any = await updateUserByIdDb(
+      user_id,
+      user_name,
+      user_address,
+      user_email,
+      user_password,
+      user_balance
+    );
+    const dataUser: any = await getUsersByIdDb(user_id);
+    if (updateUser.affectedRows == 0) {
+      return res.status(400).json({ message: "Invalid input data" });
     } else {
-        return res.status(400).json({ message: 'Invalid input data' })
+      res.status(200).json({ message: "User updated", user: dataUser });
     }
-
-    res.status(200).json({ message: 'User username updated', user: userData[userIndex] });
+  } catch (error: any) {
+    console.log("Error :", error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
-export const updateBalanceUser = (req: Request, res: Response) => {
-    const userId = parseInt(req.params.userId);
-    const { balance } = req.body;
+export const updateUsernameUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user_id = parseInt(req.params.user_id);
+  const { user_name } = req.body;
 
-    const userIndex = userData.findIndex(user => user.userId === userId);
-
-    if (userIndex === -1) {
-        return res.status(404).json({ message: 'User not found' });
-    }
-
-    if (balance !== undefined) {
-        userData[userIndex].balance = balance;
+  try {
+    const updateUser: any = await updateUsernameUserDb(user_id, user_name);
+    const dataUser: any = await getUsersByIdDb(user_id);
+    if (updateUser.affectedRows == 0 || updateUser.affectedRows == undefined) {
+      return res.status(400).json({ message: "Invalid input data" });
     } else {
-        return res.status(400).json({ message: 'Invalid input data' })
+      res.status(200).json({ message: "Username updated", user: dataUser });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
-    res.status(200).json({ message: 'User balance updated', user: userData[userIndex] });
+export const updateBalanceUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user_id = parseInt(req.params.user_id);
+  const { user_balance } = req.body;
+
+  try {
+    const updateUser: any = await updateBalanceUserDb(user_id, user_balance);
+    const dataUser: any = await getUsersByIdDb(user_id);
+    if (updateUser.affectedRows == 0) {
+      return res.status(400).json({ message: "Invalid input data" });
+    } else {
+      res.status(200).json({ message: "Username updated", user: dataUser });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { listUser, updateBalanceUser, getUserById, updateUsernameUser } from "../controllers/userController";
+import { listUser, updateBalanceUser, getUserById, updateUsernameUser, updateUserById } from "../controllers/userController";
 
 const router = express.Router();
 
 router.get("/", listUser);
-router.patch("/:user_id", updateBalanceUser);
-router.put("/:user_id", updateUsernameUser);
 router.get("/:user_id", getUserById);
+router.put("/:user_id", updateUserById);
+router.patch("/balance/:user_id", updateBalanceUser);
+router.patch("/username/:user_id", updateUsernameUser);
 
 export default router;
